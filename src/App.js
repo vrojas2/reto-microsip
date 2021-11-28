46015a3dfca0d9e4b5cb143ae73fde11ec4bb09c
vrojas2 <vrojas2@ucol.mx>
2021-11-28 13:06:17 -0600

    limpieza

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import './styles/styles.scss';
-// import './components/CRUD'
+import React from 'react';
 
 import { CRUD } from "./components/CRUD";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rama de Victor
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <CRUD/>
     </div>
   );
--- src/components/CRUD.js
+++ src/components/CRUD.js
@@ -1,24 +1,28 @@
 import React, { useContext } from 'react';
 
-import logo from '../logo.svg';
-
+// import logo from '../logo.svg';
 
 export const CRUD = () => {
 
     return (
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Rama de Victor
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
+        <div>
+            <h3>
+                Este es mi componte principal
+            </h3>
+        </div>
+        // <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //         Rama de Victor
+        //     </p>
+        //     <a
+        //         className="App-link"
+        //         href="https://reactjs.org"
+        //         target="_blank"
+        //         rel="noopener noreferrer"
+        //     >
+        //         Learn React
+        //     </a>
+        // </header>
     )
 }
